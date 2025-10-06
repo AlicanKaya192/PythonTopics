919b63b09f1238f3205305c12a9e1424bc8d814e
AlicanKaya192 <alicankaya268@gmail.com>
2025-10-07 01:57:06 +0300

    Pandas bitti.

--- Pandas/data_analysis_pandas.py
+++ Pandas/data_analysis_pandas.py
@@ -24,6 +24,7 @@
 ##################
 
 import pandas as pd
+from pandas import set_option
 
 s = pd.Series([10, 77, 12, 4, 5])
 
@@ -296,26 +297,239 @@ df_new["embark_town"].value_counts()
 # Toplulaştırma ve Gruplama (Aggretion & Grouping)
 ############################
 
-# - count()
-# - first()
-# - last()
-# - mean()
-# - median()
-# - min()
-# - nax()
-# - std()
+# count()     # Değer sayısı (NaN hariç)
+# first()     # İlk değer
+# last()      # Son değer
+# mean()      # Ortalama
+# median()    # Medyan
+# min()       # En düşük değer
+# max()       # En yüksek değer
+# std()       # Standart sapma
+# var()       # Varyans
+# sum()       # Toplam
+# - pivot table
+
+# Buradaki fonksiyonlar toplulaştırma fonksiyonlarıdır. Kullancak olduğumuz groupby() işlemiyle hepsi kullanılabilirdir. pivot table hariç.
+
+import pandas as pd
+import seaborn as sns
+pd.set_option('display.max_columns', None)
+df = sns.load_dataset("titanic")
+df.head()
+
+df["age"].mean() # Bu şekilde yaş ortalaması alırız. Fakat eğer cinsiyete göre istersek...
+
+df.groupby("sex")["age"].mean() # Yaş değişkeninin ortalamasını aldık.
+
+# Diyelim ki sadece ortalamasını değil bir de toplamını da almak istiyorum.
+
+df.groupby("sex").agg({"age": "mean"}) # Bu kullanıma alışmak bir öncekinden çok daha önemlidir.
+
+df.groupby("sex").agg({"age": ["mean", "sum"]})
+
+df.groupby("sex").agg({"age": ["mean", "sum"],
+                       "survived": "mean"})
+
+df.groupby(["sex", "embark_town"]).agg({"age": "mean",
+                                        "survived": "mean"})
+
+df.groupby(["sex", "embark_town", "class"]).agg({"age": "mean",
+                                                 "survived": "mean"})
+
+df.groupby(["sex", "embark_town", "class"]).agg({"age": "mean",
+                                                 "survived": "mean",
+                                                 "sex": "count"})
 
 
+############################
+# Pivot Table
+############################
+import pandas as pd
+import seaborn as sns
+pd.set_option('display.max_columns', None)
+df = sns.load_dataset("titanic")
+df.head()
+
+# Pivot table groupby işlemlerine benzer şekilde veri setini kırılımlar açısından değerlendirmek ve ilgilendiğimiz özet
+# istatistiği bu kırılımlar açısından görme imkanı sağlar.
+
+df.pivot_table("survived", "sex", "embark_town")
+# pivot table der ki;
+# Benim 1. argüman values argümanı, bana kesişimlerde neyi görmek istiyorsan onu gir.
+# 2. argüman index'te yani satırda hangi değişkeni görmek istiyorsun onu gir.
+# 3. argüman peki sütunda hangi değişkeni görmek istiyorsun der.
+
+# pivot_table'ın ön tanımlı değeri mean'dir yani ortalamadır. Bundan dolayı survived değişkeninin ortalaması gelecektir.
+
+df.pivot_table("survived", "sex", "embark_town", aggfunc="std")
+
+df.pivot_table("survived", "sex", ["embark_town", "class"])
+# "survived" sütununu özetliyoruz (aggfunc default olarak mean)
+# Satırda "sex" sütununa göre groupluyor
+# Sütunlarda ["embark_town", "class"] ikilisi ile çok seviyeli sütun oluşturuyor
+# Yani her embark_town ve class kombinasyonu için ortalama survived değerini gösteriyor
+
+df["new_age"] = pd.cut(df["age"], [0, 10, 18, 25, 40, 90])
+
+# cut ve qcut fonksiyonları elimizdeki sayısal değişkenleri kategorik değişkene çevirmek için en yaygın kullanılan iyi ayrı fonksiyondur.
+# Genelde sayısal değişkeni hangi kategorilere bölmek istediğinizi biliyorsanız ve bunu belirtmek isterseniz bu durumda cut() fonksiyonu kullanılır.
+# Eğer elinizdeki sayısal değişkeni tanımıyorsanız dolayısıyla çeyreklik değerlerine göre bölünsün istiyorsanız bu durumda qcut() kullanılır.
+# "Bu ne demek ?" Örneğin; yaş değişkenini kategorik değişkene çevirmek istediğinizde age değişkeni adındaki değişkeni tanıyorum.
+# Yani örneğin, diyebilirim ki; "0 ile 10 arasına çocuk de, 10 ile 18 arasına genç de, 18 ile 28 arasına da genç de. Ya da işte daha
+# büyüklerine olgun de." diyebiliyorum.
+
+# Örneğin age değişkenini tanıyorum. Dolayısıyla bu sayısal değişkeni kategorik değişkene çevirirken, çevirecek olduğum sınıfları tanımlayabiliyorum.
+# "Tanımlayamazsam ne olacak?" İşte tamamlayamazsam qcut fonksiyonunu kullanmam lazım ki, qcut() fonksiyonu otomatik olarak değerleri küçükten büyüğe
+# doğru sıralar ve yüzdelik çeyrek değerlerine göre bunları gruplara böler, kategorilere böler. Çok yaygın kullanılır.
+
+# Peki yaş kırılımlarında hayatta kalanları inceleyebilir miyiz?
+
+df["new_age"] = pd.cut(df["age"], [0, 10, 18, 25, 40, 90])
+df.pivot_table("survived", "sex", ["new_age", "class"])
+
+pd.set_option('display.width', 500) # Gelen kodları daha fazla görebilmek için
 
 
+############################
+# Apply ve Lambda
+############################
+import pandas as pd
+import seaborn as sns
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = sns.load_dataset("titanic")
+df.head()
+
+# apply() satır ya da sütunlar'da otomatik olarak fonksiyon çalıştırma imkanı sağlar. Yani bir dataframe'e apply ile istediğimiz bir fonksiyonu uygulayabiliriz.
+# satır ya da sütunlar'da uygulabiliriz.
+
+# "Lambda ne işe yarar." lambda() bir fonksiyon tanımlama şeklidir. Tıpkı def gibi. Fakat farkı kullan-at fonksiyondur.
+# Yani kod akışı esnasında bir kere kullanayım atayım daha sonra işim yok diyebileceğimiz noktalarda fonksiyon tanımlamak yerine,
+# fonksiyon tanımlamadan kullan-at bir şekilde fonksiyon kullanma imkanı sağlar. Şimdi hemen ilerleyelim.
+
+df["age2"] = df["age"] * 2
+df["age3"] = df["age"] * 5
+
+# Diyelim ki, bu veri seti içerisindeki age değişkenlerinin 10'a bölünmesini istiyoruz. "Normalde bunu nasıl gerçekleştiririz."
+# Tek tek bu değişkenleri seçeriz.
+
+# df["age"] / 10.head # Bu şekilde head kullanamayız.
+(df["age"] / 10).head() # Bu şekilde head kullanabiliriz.
+(df["age2"] / 10).head()
+(df["age3"] / 10).head()
+
+for col in df.columns:
+    if "age" in col:
+        print(col)
+
+for col in df.columns:
+    if "age" in col:
+        print((df[col] / 10).head())
+
+for col in df.columns:
+    if "age" in col:
+        df[col] = df[col] / 10
+
+# apply ve lambda ile nasıl yaparız
+
+df[["age", "age2", "age3"]].apply(lambda x: x / 10).head()
+
+# az önceki gibi programatik hale getirelim
+
+df.loc[:, df.columns.str.contains("age")].apply(lambda x: x / 10).head()
+
+df.loc[:, df.columns.str.contains("age")].apply(lambda x: (x - x.mean()) / x.std()).head()
+
+# DataFrame'deki sütun adları arasında "age" ifadesini içeren tüm sütunları seçer.
+# Daha sonra her bir seçilen sütun (x) için standartlaştırma işlemi uygular:
+# (x - x.mean()) / x.std() → yani her değerden sütunun ortalamasını çıkarır ve
+# sonucu sütunun standart sapmasına böler.
+# Bu işlem her "age" sütununu ortalaması 0, standart sapması 1 olacak şekilde ölçekler.
+# Son olarak, .head() metodu ile bu dönüştürülmüş verinin ilk 5 satırını gösterir.
+
+# Peki lambda tarafında, lambda yerine normal tanımlanmış fonksiyon kullanabilir miyiz?
+
+def standart_scaler(col_name):
+    return (col_name - col_name.mean()) / col_name.std()
+
+df.loc[:, df.columns.str.contains("age")].apply(standart_scaler).head()
+
+# Anlaşılması gereken temel nokta şudur; apply fonksiyonu bize satırlarda ya da sütunlarda elimizdeki belirli bir fonksiyonu,
+# bu satır ya da sütunlara uygulama imkanı sağlar ve bu oldukça değerlidir.
+
+# Peki bu yapılan işlemleri kaydetmek istiyorsak...
+
+df.loc[:, ["age", "age2", "age3"]] = df.loc[:, df.columns.str.contains("age")].apply(standart_scaler).head()
+
+df.loc[:, df.columns.str.contains("age")] = df.loc[:, df.columns.str.contains("age")].apply(standart_scaler).head()
 
 
+############################
+# Birleştirme İşlemleri (Join) İşlemleri
+############################
+import numpy as np
+import pandas as pd
+
+m = np.random.randint(1, 30, size=(5, 3))
+
+df1 = pd.DataFrame(m, columns=["var1", "var2", "var3"])
+# 0'dan dataframe oluşturmaya yarar.
+# 1.argümanına veri yapısı veriyoruz. Bu liste olabilir, sözlük olabilir, numpy array'i olabilir. Bunu dataframe'e çevirir.
+# 2 argümanada değişkenlerin isimlerini gireriz. Biz 5'e 3'lük oluşturduğumuz için 3 tane sütun olacak.
+
+df2 = df1 + 99
+# Burada ise df1'in değerlerinin hepsine 99 ekledik ve 2. dataframe'i oluşturduk.
+
+# 2 şekilde birleştirme yapacağız. 1. hızlı, seri, pratik ve yaygınca kullanım alanı bulan concat() metodunu kullanacağız.
+# Diğeri ise merge() yöntemi olacak.
+
+# Eğer 2 dataframe'i alt alta birleştirmek istersek.
+pd.concat([df1, df2])
+
+# İki DataFrame'i (df1 ve df2) dikey olarak (varsayılan olarak satır bazında) birleştirir.
+# Ancak DİKKAT!!! — bu birleştirme işlemi sırasında, her iki DataFrame'in de kendi index numaraları korunur.
+# Yani index değerleri çakışabilir veya sıralı olmayabilir.
+
+# Eğer yeni bir sıralı index isteniyorsa, "ignore_index=True" parametresi kullanılmalıdır:
+pd.concat([df1, df2], ignore_index=True)
+# Ön tanım değeri 0 olduğundan, pd.concat() fonksiyonu varsayılan olarak satır (axis=0) bazında birleştirme yapar.
+# Yani df2, df1'in altına eklenir. Eğer sütun bazında birleştirme istenirse axis=1 olarak belirtilmelidir.
 
 
+##################################
+# Merge ile Birleştirme İşlemleri
+##################################
 
+# Bu da daha detaylı olarak çeşitli birleştirme işlemleri yapılmasına imkan tanır.
 
+df1 = pd.DataFrame({"employees": ["john", "dennis", "mark", "maria"],
+                    "group": ["accounting", "engineering", "engineering", "hr"]})
 
+df2 = pd.DataFrame({"employees": ["mark", "john", "dennis", "maria"],
+                    "start_date": [2010, 2009, 2014, 2019]})
 
+# Elimde 2 tane dataframe var çalışan, group ve başlangıç tarihlerini bir arada istiyorum.
+
+pd.merge(df1, df2)
+# Ortak sütun adlarına göre iki DataFrame'i birleştirir (INNER JOIN mantığıyla).
+# Yani her iki DataFrame’de de ortak olan sütun değerleri üzerinden eşleşme yapılır.
+# Ortak sütun adı belirtilmezse, otomatik olarak aynı isimli sütunlar birleştirme için kullanılır.
+
+pd.merge(df1, df2, on="employee")
+# Birleştirme işleminin "employee" sütunu üzerinden yapılacağını açıkça belirtir.
+# Yani df1 ve df2 içinde "employee" sütunundaki eşleşen değerlere göre satırlar birleştirilir.
+
+# Amaç: Her çalışanın müdürünün bilgisine erişmek istiyoruz.
+df3 = pd.merge(df1, df2)
+
+df4 = pd.DataFrame({
+    'group': ['accounting', 'engineering', 'hr'],
+    'manager': ['Caner', 'Mustafa', 'Berkcan']})
+
+# Bu dataframe'de müdür bilgisi var fakat çalışan bilgisi yok, grup bilgisi var.
+# Ben eğer diğer tabloya bu tabloyu getirmek istiyorsam 2 tabloyu gruba göre birleştirmeliyim.
+
+pd.merge(df3, df4)
 
 
 
